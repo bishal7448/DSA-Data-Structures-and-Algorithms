405ebae1babb57a09f92825745d5d4b54595931d
Bishal Saha <saha.bishal7676@gmail.com>
2024-08-07 11:33:51 +0530

    Update stack_with_array.c

--- stack_with_array.c
+++ stack_with_array.c
@@ -3,61 +3,61 @@
 
 #define size 10
 int stack[size];
-int top=-1;
+int top = -1;
 
-void push(){
-  if (top==size-1){
-    printf("The stack is overflow.\n");
-  }else{
-      int data;
-      printf("Enter the data want to push: ");
-      scanf("%d", &data);
-      top++;
-      stack[top]=data;
+void push() {
+  if (top == size - 1) {
+    printf("Stack is oveflow.\n");
+  } else {
+    int data;
+    printf("Enter the data to be pushed: ");
+    scanf("%d", &data);
+    top++;
+    stack[top] = data;
   }
 }
 
-void pop(){
-  if(top==-1){
-    printf("The stack is underflow.\n");
-  }else{
-    int backup=stack[top];
+void pop() {
+  if (top == -1) {
+    printf("Stack is underflow.\n");
+  } else {
+    int backup = stack[top];
     top--;
-    printf("The popped data is %d.\n", backup);
+    printf("The popped element is %d.\n", backup);
   }
 }
 
-void display(){
-  if(top==-1){
-    printf("The stack is underflow.\n");
-  }else{
-    for (int i=top; i>=0; top--){
-      printf("%d", stack[i]);
+void display() {
+  if (top == -1) {
+    printf("Stack is underflow.\n");
+  } else {
+    for (int i = top; i >= 0; i--) {
+      printf("%d\n", stack[i]);
     }
   }
 }
 
-int main(){
-  while (1){
-    char ch;
-    printf("Enter your choice ('1' for push, '2' for pop, '3' for display, '4' for exit): ");
-    scanf("%c", &ch);
-    switch (ch) {
-      case '1' :
-        push();
-        break;
-      case '2' :
-        pop();
-        break;
-      case '3' :
-        display();
-        break;
-      case '4' :
-        exit(0);
-        break;
-      default :
-        printf("Enter a valid choice.\n");
+int main() {
+  do {
+    int c;
+    printf("Enter the choice: ");
+    scanf("%d", &c);
+    switch (c) {
+    case 1:
+      push();
+      break;
+    case 2:
+      pop();
+      break;
+    case 3:
+      display();
+      break;
+    case 4:
+      exit(0);
+      break;
+    default:
+      printf("Enter a valid choiice.\n");
     }
-  }
-return 0;
+  } while (1);
+  return 0;
 }
