7a76a2a32b01f3db7fc4e53131b330a4acb2b42b
Bishal Saha <saha.bishal7676@gmail.com>
2024-08-09 22:15:47 +0530

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 
-int create_array(int array[], int size);
-int bubble_sort(int array[], int size);
-int display_array(int array[], int size);
+void create_array(int array[], int size);
+void bubble_sort(int array[], int size);
+void display_array(int array[], int size);
 
 int main(){
-  
+
   int size;
   printf("Enter the size of the array: ");
   scanf("%d", &size);
   int arr[size];
-  
+
   create_array(arr, size);
   bubble_sort(arr, size);
   display_array(arr, size);
-  
+
 return 0;
 }
 
-int create_array(int array[], int size){
+void create_array(int array[], int size){
   for (int i=0; i<size; i++){
-    printf("Enter the %d index valude: ", i);
+    printf("Enter the value of %d index position: ", i);
     scanf("%d", &array[i]);
   }
 }
 
-int bubble_sort(int array[], int size){
+void bubble_sort(int array[], int size){
   for (int i=0; i<size; i++){
     for (int j=i+1; j<size; j++){
       if (array[i]>array[j]){
-          int temp=array[i];
-          array[i]=array[j];
-          array[j]=temp;
+        int temp=array[i];
+        array[i]=array[j];
+        array[j]=temp;
       }
     }
   }
 }
 
-int display_array(int array[], int size){
+void display_array(int array[], int size){
+  printf("The sorted array:\n");
   for (int i=0; i<size; i++){
     printf("%d\n", array[i]);
   }
