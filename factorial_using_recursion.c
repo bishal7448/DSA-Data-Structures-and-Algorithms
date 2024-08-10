d2ceb299483d02082268cf874fe2b6992412ec47
Bishal Saha <saha.bishal7676@gmail.com>
2024-08-10 14:56:51 +0530

    Update factorial_using_recursion.c

--- factorial_using_recursion.c
+++ factorial_using_recursion.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int factorial(int n);
+
+int main(){
+  int n;
+  printf("Enter the number want to get factorial: ");
+  scanf("%d", &n);
+  int fact=factorial(n);
+  printf("The factorial of %d is %d.", n, fact);
+}
+
+int factorial(int n){
+  if (n==0){
+    return 1;
+  }else{
+    return n*factorial(n-1);
+  }
+}
