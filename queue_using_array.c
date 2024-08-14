ff0bffcfb5f2e43f83308510545fcae10be3f07b
Bishal Saha <saha.bishal7676@gmail.com>
2024-08-14 12:06:11 +0530

    Update queue_using_array.c

--- queue_using_array.c
+++ queue_using_array.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#define size 10
+int queue[size];
+int rare=-1;
+int front=-1;
+
+void enqueue(){
+    if(rare==size-1){
+        printf("Queue is overflow.\n");
+    }else if(front==-1 && rare==-1){
+        int item;
+        printf("
+        front++;
+        rare++;
