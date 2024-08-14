92661f690fe0c85dad2ae21bfb0def3c48588a05
Bishal Saha <saha.bishal7676@gmail.com>
2024-08-14 12:24:18 +0530

    Update queue_using_array.c

--- queue_using_array.c
+++ queue_using_array.c
@@ -11,6 +11,38 @@ void enqueue(){
         printf("Queue is overflow.\n");
     }else if(front==-1 && rare==-1){
         int item;
-        printf("
+        printf("Enter value want to insert:");
+        scnaf("%d", &item);
         front++;
         rare++;
+        queue[rare]=item;
+    }else{
+        int item;
+        printf("Enter value want to insert:");
+        scnaf("%d", &item);
+        rare+1;
+        queue[rare]=item;
+    }
+}
+
+void dequeue(){
+    if(front==-1 && rare==-1){
+        printf("Queue is empty.\n");
+    }else{
+        int back=queue[front];
+        front--;
+        printf("The dequeue value is %d.\n");
+    }
+}
+
+void display(){
+    if(front==-1 && rare==-1){
+        printf("Queue is empty.\n");
+    }else{
+        printf("The array:\n");
+        for(int i=0; i<size; i++){
+            printf("%d", queue[i]);
+        }
+    }
+}
+        
