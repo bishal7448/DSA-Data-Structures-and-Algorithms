f8e4dc9cf16701fb2a4710259809cbf39cd3cbc5
Bishal Saha <saha.bishal7676@gmail.com>
2024-11-26 01:26:42 +0530

    Update stack_using_array.c

--- stack_using_array.c
+++ stack_using_array.c
@@ -5,6 +5,40 @@
 int stack[size];
 int top=-1;
 
+void push();
+void pop();
+void display();
+void peek();
+
+int main(){
+    do{
+        printf("Enter 1 for push, 2 for pop, 3 for display, 4 for peek, 5 for exit.\n");
+        int choice;
+        printf("Enter your choice:");                    //Enter your choice
+        scanf("%d", &choice);
+        switch(choice){                    //Cases
+            case 1:
+                push();
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                peek();
+                break;
+            case 5:
+                exit(0);
+                break;
+            default:
+                printf("Enter a valid choice.\n");
+        }
+    }while(1);                    //Loop condition.
+    return 0;
+}
+
 void push(){                    //Push some value in top of the array
     if(top==size-1){
         printf("Stack is overflow.\n");
@@ -43,33 +77,4 @@ void peek(){                    //Get the top value of the present array
     }else{
         printf("The top value is %d\n", stack[top]);
     }
-}
-
-int main(){
-    do{
-        printf("Enter 1 for push, 2 for pop, 3 for display, 4 for peek, 5 for exit.\n");
-        int choice;
-        printf("Enter your choice:");                    //Enter your choice
-        scanf("%d", &choice);
-        switch(choice){                    //Cases
-            case 1:
-                push();
-                break;
-            case 2:
-                pop();
-                break;
-            case 3:
-                display();
-                break;
-            case 4:
-                peek();
-                break;
-            case 5:
-                exit(0);
-                break;
-            default:
-                printf("Enter a valid choice.\n");
-        }
-    }while(1);                    //Loop condition.
-    return 0;
 }
